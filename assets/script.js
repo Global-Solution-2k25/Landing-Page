be8a59d57c451e3e1ed3a1eef85732e5acde06b7
Arthur Berlofa Bosi <berlofabosi.arthur@gmail.com>
2025-06-06 22:51:44 -0300

    fix: resetState()

--- assets/script.js
+++ assets/script.js
@@ -95,3 +95,38 @@ const questionElement = document.getElementById("quizQuestion");
 const answerButtons = document.getElementById("quizAnswerBtns");
 const nextButton = document.getElementById("quizNextBtn");
 
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Próxima";
+  showQuestion();
+}
+
+function showQuestion() {
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+
+    button.dataset.id = answer.id;
+
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
